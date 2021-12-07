d34c2216cda8be34557808a59f0c787df4625814
Naz <nazzzarr@gmail.com>
2021-12-07 10:31:40 +0200

    fix migration config

--- src/database/config.ts
+++ src/database/config.ts
@@ -1,11 +1,11 @@
 module.exports = {
   development: {
     dialect: 'postgres',
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    username: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DB,
+    host: 'localhost',
+    port: 5432,
+    username: 'postgres',
+    password: 'root',
+    database: 'myproj4',
   },
   test: {
     dialect: 'postgres',
